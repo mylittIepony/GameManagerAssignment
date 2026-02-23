1e5a67f8359c2feca05582fe3e1d70952d156b46
pony <mercedeshammond27@Gmail.com>
2026-02-23 15:33:20 -0700

    weapon or whatever

---
+++ GameManager/Assets/Rainbow Magic/Demo/Scripts/RainbowSceneSelect.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RainbowMagic
+{
+
+public class RainbowSceneSelect : MonoBehaviour
+{
+	public bool GUIHide = false;
+	public bool GUIHide2 = false;
+	public bool GUIHide3 = false;
+	
+    public void LoadSceneDemo1()
+    {
+        SceneManager.LoadScene("RainbowMissiles");
+    }
+    public void LoadSceneDemo2()
+    {
+        SceneManager.LoadScene("RainbowDemo01");
+    }
+    public void LoadSceneDemo3()
+    {
+        SceneManager.LoadScene("RainbowDemo02");
+    }
+    public void LoadSceneDemo4()
+    {
+        SceneManager.LoadScene("RainbowDemo03");
+    }
+    public void LoadSceneDemo5()
+    {
+        SceneManager.LoadScene("RainbowDemo04");
+    }
+    public void LoadSceneDemo6()
+    {
+        SceneManager.LoadScene("RainbowDemo05");
+    }
+	
+	void Update ()
+	 {
+ 
+     if(Input.GetKeyDown(KeyCode.L))
+	 {
+         GUIHide = !GUIHide;
+     
+         if (GUIHide)
+		 {
+             GameObject.Find("CanvasSceneSelect").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("CanvasSceneSelect").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+	      if(Input.GetKeyDown(KeyCode.J))
+	 {
+         GUIHide2 = !GUIHide2;
+     
+         if (GUIHide2)
+		 {
+             GameObject.Find("Canvas").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("Canvas").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+		if(Input.GetKeyDown(KeyCode.H))
+	 {
+         GUIHide3 = !GUIHide3;
+     
+         if (GUIHide3)
+		 {
+             GameObject.Find("CanvasTips").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("CanvasTips").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+	}
+}
+}
---
+++ GameManager/Assets/Rainbow Magic/Rainbow Magic - Readme.txt
@@ -0,0 +1,21 @@
+------------------
+Rainbow Magic by Archanor VFX
+------------------
+
+The effect prefabs can simply be dragged and dropped into the scene, or spawned with a script.
+
+Feel free to reference or re-purpose any of the included scripts for your games or projects.
+
+If you upgrade this asset to URP, keep in mind that Additive materials may look slightly different compared to the Built-in Render Pipeline.
+
+------------------
+Common Issues (Invisible Particles)
+------------------
+
+* If your particles are invisible or don't render in a build, try turning off Soft Particles.
+
+------------------
+
+Need help? Visit: https://archanor.com/support.html
+
+If you like this asset, please rate and review it at the Asset Store!
